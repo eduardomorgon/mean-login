9572dc547b10430d0f301901ba52686d6e26dcce
Eduardo Morgon <eduardo.morgon@gmail.com>
2016-08-16 20:54:08 -0300

    alterado form cadastro usuario

--- server/config/app.js
+++ server/config/app.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'palavrachave',
-    'db': 'mongodb://localhost/dbusuario'
+    'db': 'mongodb://teste:teste@ds015915.mlab.com:15915/mongodb-user'
 };
 
 //mongodb://localhost/dbusuario
